732df0ec4d4553166511aea4cc5355209ef40d01
Maxime Study <maxime.study@gmail.com>
2018-12-05 15:27:49 -0500

    refactor name

--- src/main/java/ca/ulaval/glo4002/accountbilling/AccountBillingService.java
+++ src/main/java/ca/ulaval/glo4002/accountbilling/AccountBillingService.java
@@ -5,32 +5,36 @@ import java.util.List;
 public class AccountBillingService {
 
 	public void cancelInvoiceAndRedistributeFunds(BillId id) {
-		Bill bill = BillDAO.getInstance().findBill(id);
-		if (!(bill == null)) {
-			ClientId cid = bill.getClientId();
+		Bill billToCancel = BillDAO.getInstance().findBill(id);
+		if (!(billToCancel == null)) {
+			ClientId clientId = billToCancel.getClientId();
 
-			if (bill.isCancelled() != true) bill.cancel();
-			BillDAO.getInstance().persist(bill);
+			if (!billToCancel.isCancelled())
+				billToCancel.cancel();
+			
+			BillDAO.getInstance().persist(billToCancel);
 
-			List<Allocation> a = bill.getAllocations();
+			List<Allocation> allocations = billToCancel.getAllocations();
 
-			for (Allocation al : a) {
-				List<Bill> bills = BillDAO.getInstance().findAllByClient(cid); int amount = al.getAmount();
+			for (Allocation allocation : allocations) {
+				List<Bill> bills = BillDAO.getInstance().findAllByClient(clientId);
+				int amount = allocation.getAmount();
 
-				for (Bill b : bills) {
-					if (bill != b) {
-						int remainingAmount = b.getRemainingAmount();
-						Allocation allocation;
-						if (remainingAmount <= amount) { allocation = new Allocation(remainingAmount);
+				for (Bill bill : bills) {
+					if (billToCancel != bill) {
+						int remainingAmount = bill.getRemainingAmount();
+						Allocation newRedistributedAllocation;
+						if (remainingAmount <= amount) {
+							newRedistributedAllocation = new Allocation(remainingAmount);
 							amount -= remainingAmount;
 						} else {
-							allocation = new Allocation(amount);
-					amount = 0;
+							newRedistributedAllocation = new Allocation(amount);
+							amount = 0;
 						}
 
-						b.addAllocation(allocation);
-						
-						BillDAO.getInstance().persist(b);
+						bill.addAllocation(newRedistributedAllocation);
+
+						BillDAO.getInstance().persist(bill);
 					}
 
 					if (amount == 0) {
